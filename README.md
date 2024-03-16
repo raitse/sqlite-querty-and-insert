8485c2d494776045cc82596675568c230e28f539
Tommi Raitanen <d0500502@dna-km151.wlan>
2024-03-16 18:46:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# sqlite-querty-and-insert
---
+++ generator.py
@@ -0,0 +1,43 @@
+import os
+import random
+import libsql_experimental as libsql
+from dotenv import load_dotenv
+from datetime import datetime
+
+
+# 5k riviä yhden random ID kohdalle
+# Timestamp pitää olla nykyhetki (Created at kolumni)
+# Joku muu random yksi ID 3k riviä
+# loput 4k riviä muihin id:hen, ei kahteen aiempaan
+
+
+def main():
+    counter = 0
+    print(f"connecting to {os.getenv('LIBSQL_URL')}")
+    conn = libsql.connect(
+        database=os.getenv("LIBSQL_URL"), auth_token=os.getenv("LIBSQL_AUTH_TOKEN")
+    )
+    conn.execute("BEGIN TRANSACTION")
+    rows = conn.execute("SELECT * FROM users;").fetchall()
+    for row in rows:
+        user_id = row[0]
+        counter += 1
+        if counter <= 1:
+            user_insert_count = range(3000)
+        elif counter == 2:
+            user_insert_count = range(1000)
+        else:
+            random_insert_count = random.randint(100, 200)
+            user_insert_count = range(random_insert_count)
+        timestamp = datetime.now()
+        message = "test_message"
+        for _ in user_insert_count:
+            conn.execute(
+                f"INSERT INTO message (created_at, target_id, message) VALUES ('{timestamp}', '{user_id}', '{message}');"
+            )
+            conn.execute("END TRANSACTION")
+    conn.commit()
+
+
+load_dotenv()
+main()
